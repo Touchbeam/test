2377744daf513305f2059bb87bb5c6be5887c491
Touchbeam <sjh3285@gmail.com>
2025-01-20 09:33:48 +0900

    Update serverEnv.js

--- src/serverEnv.js
+++ src/serverEnv.js
@@ -3,9 +3,9 @@
     testhost : "http://192.168.0.2:8010"
 */
 
-export const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST || "http://localhost:8010"; 
+export const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST || "hhttp://192.168.0.87:8010/"; 
 
-export const REDIRECT_HOST = process.env.REACT_APP_REDIRECT_HOST || "http://localhost:3010";
+export const REDIRECT_HOST = process.env.REACT_APP_REDIRECT_HOST || "https://fastidious-toffee-be993e.netlify.app/";
 
 export const REST_API_KEY = process.env.REACT_APP_REST_API_KEY || "7cbd76ba5d**********aee484d9ceb3";
 
